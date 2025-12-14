b3fcdd29749280386ce70da827d715cbbb30e7ec
PrinceDev <sentiboi1025@gmail.com>
2025-12-15 00:50:21 +0800

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,6 @@ export const POKTPOOL_STRING = 'poktpool™'
 
 // Auth expiration configuration (in milliseconds)
 export const AUTH_CONFIG = {
-  ACCESS_TOKEN_EXPIRES_IN: 3600, // 1 hour in milliseconds
+  ACCESS_TOKEN_EXPIRES_IN: 36000, // 10 hour in milliseconds
   JWT_MAX_AGE: 60 * 29, // 29 minutes in seconds
 }
